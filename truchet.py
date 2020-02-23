dd06a45a96578cd1885659c0934efeb930be6e8f
Evan Balzuweit <ebalzuweit@gmail.com>
2020-02-23 11:15:28 -0600

    run based on directories instead of files

--- truchet.py
+++ truchet.py
@@ -1,5 +1,4 @@
-import sys
-import random
+import sys, os, random
 from PIL import Image
 from argparse import ArgumentParser
 
@@ -41,37 +40,48 @@ def burst_scheme(tile, i, x, y):
 
 def main():
     argparser = ArgumentParser()
-    argparser.add_argument('-t', '--tile', help='the tile file to use', type=str, required=True)
-    argparser.add_argument('-o', '--output', help='the output file to save', type=str, default='output.png')
-    argparser.add_argument('-x', '--width', help='the width of the output', type=int, default=512)
-    argparser.add_argument('-y', '--height', help='the height of the output', type=int, default=512)
+    argparser.add_argument('-t', '--tile_dir', help='tile directory', type=str, required=True)
+    argparser.add_argument('-o', '--output_dir', help='output directory', type=str, default='./output')
+    argparser.add_argument('-x', '--width', help='the width of the output images', type=int, default=512)
+    argparser.add_argument('-y', '--height', help='the height of the output images', type=int, default=512)
     args = argparser.parse_args()
 
-    # removed appended .png if present
-    if (args.output[-4:] == '.png'):
-        args.output = args.output[:-4]
-
-    # open tile image
-    try:
-        tile = Image.open(args.tile)
-    except IOError:
-        print(f'Unable to open tile file: {args.tile}')
+    if not os.path.isdir(args.tile_dir):
+        print(f'Not a valid directory: {args.tile_dir}')
         sys.exit(1)
-    
-    # build schemes
-    schemes = {
-        'standard': lambda i, x, y: tile,
-        'random': lambda i, x, y: tile.rotate(90 * random.randrange(0, 3)),
-        'windmill': lambda i, x, y: windmill_scheme(tile, i, x, y),
-        'burst': lambda i, x, y: burst_scheme(tile, i, x, y),
-        'rotate90': lambda i, x, y: tile.rotate(90 * i),
-        'rotate180': lambda i, x, y: tile.rotate(180 * i),
-        'rotate270': lambda i, x, y: tile.rotate(270 * i),
-    }
+    if not os.path.isdir(args.output_dir):
+        print(f'Not a valid directory: {args.output_dir}')
 
-    # run schemes
-    for key in schemes.keys():
-        truchet(tile.size, schemes[key], f'{args.output}_{key}', (args.width, args.height))
+    tiles = []
+    for (dirpath, dirnames, filenames) in os.walk(args.tile_dir):
+        for f in filenames:
+            if f[-4:] == '.png':
+                tiles.append(f)
+        break # only looking at top directory
+    
+    for tile_file in tiles:
+        # open tile image
+        try:
+            tile = Image.open(os.path.join(args.tile_dir, tile_file))
+        except IOError:
+            print(f'Unable to open tile file: {tile_file}')
+            continue
+
+        # build schemes
+        schemes = {
+            'standard': lambda i, x, y: tile,
+            'random': lambda i, x, y: tile.rotate(90 * random.randrange(0, 3)),
+            'windmill': lambda i, x, y: windmill_scheme(tile, i, x, y),
+            'burst': lambda i, x, y: burst_scheme(tile, i, x, y),
+            'rotate90': lambda i, x, y: tile.rotate(90 * i),
+            'rotate180': lambda i, x, y: tile.rotate(180 * i),
+            'rotate270': lambda i, x, y: tile.rotate(270 * i),
+        }
+
+        # run schemes
+        for key in schemes.keys():
+            filename = os.path.join(args.output_dir, f'{tile_file[:-4]}_{key}')
+            truchet(tile.size, schemes[key], filename, (args.width, args.height))
 
 if __name__ == '__main__':
     main()
